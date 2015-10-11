7b5007122c6157064d59908a57717ed6a230c4cc
Rob Allen <rob@akrabat.com>
2015-10-11 11:20:10 +0100

    Help the PHP dev server to serve static files

--- public/index.php
+++ public/index.php
@@ -1,4 +1,13 @@
 <?php
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $file = __DIR__ . $_SERVER['REQUEST_URI'];
+    if (is_file($file)) {
+        return false;
+    }
+}
+
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
